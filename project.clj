fd4096c395626a9dd944641c834949f3d6df48b0
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2014-08-14 01:55:10 +0200

    Mark version 0.2.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject camel-snake-kebab "0.2.4-SNAPSHOT"
+(defproject camel-snake-kebab "0.2.4"
   :description "A library for word case conversions."
   :url "https://github.com/qerub/camel-snake-kebab"
   :license {:name "Eclipse Public License"
