faf5a4a4f8bc2e8c5d364246ec7a6be7675f79d5
Alex Baranosky <alexander.baranosky@gmail.com>
2014-09-12 21:22:59 -0700

    Some style tweaks

--- src/camel_snake_kebab/internals/macros.clj
+++ src/camel_snake_kebab/internals/macros.clj
@@ -4,20 +4,18 @@
 
 (defn type-preserving-function [case-label first-fn rest-fn sep]
   `(let [convert-case# (partial convert-case ~first-fn ~rest-fn ~sep)]
-     (defn ~(->> case-label (format "->%s") symbol) [s#]
+     (defn ~(symbol (str "->" case-label)) [s#]
        (camel-snake-kebab.core/alter-name s# convert-case#))))
 
 (defn type-converting-functions [case-label first-fn rest-fn sep]
   (letfn [(make-name [type-label]
-            (->> [case-label type-label]
-                 (join \space)
+            (->> (str case-label " " type-label)
                  (convert-case (resolve first-fn) (resolve rest-fn) sep)
-                 (format "->%s")
+                 (str "->")
                  (symbol)))]
     (for [[type-label type-converter] {"string" `identity "symbol" `symbol "keyword" `keyword}]
       `(defn ~(make-name type-label) [s#]
-         (->> s#
-              name
+         (->> (name s#)
               (convert-case ~first-fn ~rest-fn ~sep)
               ~type-converter)))))
 
